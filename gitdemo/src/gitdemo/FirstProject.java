cb21b5cf0f264ce84dd2472faf52d9b5d136cd40
rahul bhuktar <ravi106009@gmail.com>
2025-01-23 15:07:39 +0530

    method added

---
+++ gitdemo/src/gitdemo/FirstProject.java
@@ -0,0 +1,16 @@
+package gitdemo;
+
+public class FirstProject {
+
+	
+	public static int addNumber(int a,int b)
+	{
+		return a+b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int sum=  addNumber(20,10);
+System.out.println(sum);
+	}
+
+}
